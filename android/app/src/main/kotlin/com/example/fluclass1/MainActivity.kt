7798263c7abe3a8fb38d1383efc40209b5fb67fe
Hassan Raza <64700946+hr280@users.noreply.github.com>
2021-05-05 02:13:17 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fluclass1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluclass1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+     home: Scaffold(
+        body: Text(" Name: Hassan Raza \n Email: hr280161@gmail.com"),  
+        
+     ),
+   );
+  }
+}
